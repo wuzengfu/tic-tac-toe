9927df4827eec8758527cf326d0291690fdee7ae
wuzengfu <wuzengfu666@gmail.com>
2021-11-13 20:53:34 +0800

    change background color when exitBtn and userItem is on hover

--- frontend/src/components/UserItem.jsx
+++ frontend/src/components/UserItem.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export const UserItem = (props) => {
+    const [isHover, setIsHover] = useState(false);
+
     return (
-        <li className="list-group-item d-flex justify-content-between align-items-start">
+        <li className="list-group-item d-flex justify-content-between align-items-start" style={isHover ? styles.listOnHover : null}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+        >
             <div className="ms-2 me-auto">
                 <div className="fw-bold">{props.username}</div>
             </div>
         </li>
     );
+}
+
+const styles = {
+    listOnHover: {
+        backgroundColor: 'rgb(236,231,231)',
+        cursor: 'pointer'
+    }
 }
